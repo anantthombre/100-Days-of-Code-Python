852ae8e891a78bf4fe1591eff44e0b66996c0775
Anant Thombre <anantthombre1999@gmail.com>
2023-02-19 21:33:22 +0530

    updated day_45_main.py

--- Day 45 - Wev Scrapping with Beautiful Soup/day_45_main.py
+++ Day 45 - Wev Scrapping with Beautiful Soup/day_45_main.py
@@ -6,4 +6,14 @@ with open("website.html") as file:
 
 
 soup = BeautifulSoup(contents, "html.parser")
-print(soup.title)
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.text)
+# print(soup.title.string)
+# print(soup)
+# print(soup.prettify)
+# print(soup.a)
+# print(soup.p)
+
+
+
