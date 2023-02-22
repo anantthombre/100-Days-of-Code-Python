ae4f064aab2cd67c53a14cf1dfaedb30b2ee964a
Anant Thombre <anantthombre1999@gmail.com>
2023-02-22 21:37:12 +0530

    updated interaction.py

--- Day 48 - Selenium & Game Playing Bot/interaction.py
+++ Day 48 - Selenium & Game Playing Bot/interaction.py
@@ -7,18 +7,23 @@ import time
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("https://web.archive.org/web/20220120120408/https://secure-retreat-92358.herokuapp.com/")
 
-article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
-print(article_count.text)
+# article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+# print(article_count.text)
 #article_count.click()
 
 # wikipedia = driver.find_element(By.LINK_TEXT, "Wikipedia")
 # wikipedia.click()
 
-search = driver.find_element(By.NAME, "search")
-search.send_keys("Python")
-search.send_keys(Keys.ENTER)
+# search = driver.find_element(By.NAME, "search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
 
+driver.find_element(By.NAME, "fName").send_keys("Abcd")
+driver.find_element(By.NAME, "lName").send_keys("Efgh")
+driver.find_element(By.NAME, "email").send_keys("ijkl@mnop.qrs")
+driver.find_element(By.TAG_NAME, "button").click()
 
-time.sleep(5)
+time.sleep(10)
