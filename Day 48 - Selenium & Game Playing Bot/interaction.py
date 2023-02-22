8e71d0ef66dd24281d90221019c87da9a3403905
Anant Thombre <anantthombre1999@gmail.com>
2023-02-22 21:06:07 +0530

    updated interaction.py

--- Day 48 - Selenium & Game Playing Bot/interaction.py
+++ Day 48 - Selenium & Game Playing Bot/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
@@ -10,5 +11,14 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
 print(article_count.text)
-article_count.click()
+#article_count.click()
+
+# wikipedia = driver.find_element(By.LINK_TEXT, "Wikipedia")
+# wikipedia.click()
+
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
+
+
 time.sleep(5)
