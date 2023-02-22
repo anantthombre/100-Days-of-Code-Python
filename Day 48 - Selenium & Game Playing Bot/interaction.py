d760d614503d175b7694ec6d331fb5d984defa66
Anant Thombre <anantthombre1999@gmail.com>
2023-02-22 20:40:57 +0530

    updated interaction.py

--- Day 48 - Selenium & Game Playing Bot/interaction.py
+++ Day 48 - Selenium & Game Playing Bot/interaction.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -9,4 +10,5 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
 print(article_count.text)
-
+article_count.click()
+time.sleep(5)
