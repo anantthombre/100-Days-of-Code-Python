780fa5b278d6716948e7c9a9b8ea06615262d78c
Anant Thombre <anantthombre1999@gmail.com>
2023-03-01 22:17:28 +0530

    updated day 53 main.py

---
+++ Day 53 - House Renting Research Automation/main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import requests
+
+
+
